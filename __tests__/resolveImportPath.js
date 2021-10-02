f7d813a0b48c66fd5864bab75b4672486ab3412d
Technote <technote.space@gmail.com>
2021-10-03 00:17:06 +0900

    feat: add module test

---
+++ __tests__/resolveImportPath.js
@@ -0,0 +1,49 @@
+const resolveImportPath = require('../strict-dependencies/resolveImportPath')
+const {readFileSync} = require('fs')
+
+jest.mock('fs')
+
+describe('resolveImportPath', () => {
+  it('should do nothing if tsconfig.json does not exist', () => {
+    readFileSync.mockImplementation(() => {
+      throw new Error()
+    })
+    expect(resolveImportPath('components/aaa/bbb')).toBe('components/aaa/bbb')
+  })
+
+  it('should do nothing if no paths setting', () => {
+    readFileSync.mockReturnValue(JSON.stringify({}))
+    expect(resolveImportPath('components/aaa/bbb')).toBe('components/aaa/bbb')
+  })
+
+  it('should resolve tsconfig paths', () => {
+    readFileSync.mockReturnValue(JSON.stringify({
+      compilerOptions: {
+        paths: {
+          '@/components/': ['components/'],
+        },
+      },
+    }))
+
+    expect(resolveImportPath('components/aaa/bbb')).toBe('components/aaa/bbb')
+    expect(resolveImportPath('@/components/aaa/bbb')).toBe('components/aaa/bbb')
+  })
+
+  describe('should resolve tsconfig paths with baseUrl', () => {
+    ['src', './src', 'src/'].forEach(baseUrl => {
+      it(baseUrl, () => {
+        readFileSync.mockReturnValue(JSON.stringify({
+          compilerOptions: {
+            baseUrl,
+            paths: {
+              '@/components/': ['components/'],
+            },
+          },
+        }))
+
+        expect(resolveImportPath('components/aaa/bbb')).toBe('components/aaa/bbb')
+        expect(resolveImportPath('@/components/aaa/bbb')).toBe('src/components/aaa/bbb')
+      })
+    })
+  })
+})
